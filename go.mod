fae1f39bfb754a3e8b3f9ab0486e355d29171664
Jed Borseth <jedborseth@Jeds-MBP.va.shawcable.net>
2023-10-30 22:52:35 -0700

    random food api route

--- go.mod
+++ go.mod
@@ -2,15 +2,17 @@ module example/hello
 
 go 1.21.1
 
-require github.com/libsql/libsql-client-go v0.0.0-20231026052543-fce76c0f39a7
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
+	github.com/libsql/libsql-client-go v0.0.0-20231026052543-fce76c0f39a7
+	github.com/mrz1836/go-sanitize v1.3.1
+)
 
 require (
 	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
-	github.com/mrz1836/go-sanitize v1.3.1 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -28,6 +30,7 @@ func main() {
 	http.HandleFunc("/hello", helloRoute)
 	http.HandleFunc("/addUser", addUser)
 	http.HandleFunc("/selectAll", selectAll)
+	http.HandleFunc("/randomFood", randomFood)
 
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {
@@ -145,7 +148,7 @@ for rows.Next() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	tableRow := "<tr class='w-full flex gap-2'><td>" + fmt.Sprint(user.ID) + "</td><td>" + user.Name + "</td><td>" + user.Email + "</td></tr>"
+	tableRow := "<tr class='w-full flex gap-2'><td class='lg:w-16'>" + fmt.Sprint(user.ID) + "</td><td>" + user.Name + "</td><td>" + user.Email + "</td></tr>"
 	// list = append(list, tableRow)
 	io.WriteString(w, tableRow)
 }
@@ -155,4 +158,40 @@ for rows.Next() {
 // 		}
 // w.Write(jsonResp)
 
+}
+
+
+func randomFood(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get("https://www.themealdb.com/api/json/v1/1/random.php")
+
+	if err != nil {
+		fmt.Printf("Error getting random food: %s\n", err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %s\n", err)
+		return
+	}
+	var data FoodApiTypes
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Printf("Error unmarshalling response body: %s\n", err)
+		return
+	}
+	templ, err := template.ParseFiles("./templates/foodTemplate.html")
+	if err != nil {
+		fmt.Printf("Error parsing template: %s\n", err)
+		return
+	}
+
+	var tpl bytes.Buffer
+	err = templ.Execute(&tpl, data.Meals[0])
+	if err != nil {
+		fmt.Printf("Error executing template: %s\n", err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(tpl.Bytes())
 }
